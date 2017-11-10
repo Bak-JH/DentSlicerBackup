e72f27803cba98fec74e3ce4601cdfccca9db316
diridiri <junharin@kaist.ac.kr>
2017-11-10 21:09:10 +0900

    ADDED bisect

--- glmodel.cpp
+++ glmodel.cpp
@@ -28,35 +28,34 @@ GLModel::GLModel(QNode *parent)
     QObject::connect(timer, &QTimer::timeout,this,&GLModel::onTimerUpdate);
     QObject::connect(m_objectPicker, SIGNAL(clicked(Qt3DRender::QPickEvent*)), this, SLOT(handlePickerClicked(Qt3DRender::QPickEvent*)));
 
-    /*mesh = new Mesh();
+    mesh = new Mesh();
     loadMeshSTL(mesh, "C:/Users/diridiri/Desktop/DLP/partial1.stl");
     initialize();
-    addVertices();*/
+    addVertices();
     //addVertices(mesh->vertices);
 
     //m_mesh->setGeometry(m_geometry);
-    m_mesh->setSource(QUrl(QStringLiteral("file:///D:/Dev/DLPSlicer/DLPslicer/resource/mesh/lowerjaw.obj")));
-    //Qt3DExtras::QDiffuseMapMaterial *diffuseMapMaterial = new Qt3DExtras::QDiffuseMapMaterial();
-    //diffuseMapMaterial->setSpecular(QColor::fromRgbF(0.2f, 0.2f, 0.2f, 1.0f));
-    //diffuseMapMaterial->setShininess(2.0f);
+    //m_mesh->setSource(QUrl(QStringLiteral("file:///D:/Dev/DLPSlicer/DLPslicer/resource/mesh/lowerjaw.obj")));
+    Qt3DExtras::QDiffuseMapMaterial *diffuseMapMaterial = new Qt3DExtras::QDiffuseMapMaterial();
+    diffuseMapMaterial->setSpecular(QColor::fromRgbF(0.2f, 0.2f, 0.2f, 1.0f));
+    diffuseMapMaterial->setShininess(2.0f);
 
-    //addComponent(diffuseMapMaterial);
+    addComponent(diffuseMapMaterial);
 
-    Qt3DExtras::QPhongMaterial *phongMaterial = new Qt3DExtras::QPhongMaterial();
+    /*Qt3DExtras::QPhongMaterial *phongMaterial = new Qt3DExtras::QPhongMaterial();
     phongMaterial->setDiffuse(QColor(204, 205, 75)); // Safari Yellow #cccd4b
     phongMaterial->setSpecular(Qt::white);
 
-    addComponent(phongMaterial);
+    addComponent(phongMaterial);*/
     addComponent(m_mesh);
     addComponent(m_transform);
     addComponent(m_objectPicker);
 
+    m_mesh->setGeometry(m_geometry);
 
-    qDebug() << "1";
-    QGeometry* geometry = m_mesh->geometry();
-    qDebug() << "2";
-    auto attributes = geometry->attributes();
-    qDebug() << "3";
+
+    auto attributes = m_geometry->attributes();
+
 
     int vertexIndex = 0;
 
@@ -76,12 +75,11 @@ GLModel::GLModel(QNode *parent)
 
             // replace float with your data type
             float *value = reinterpret_cast<float*>(rawData);
-
+            qDebug() << value;
             break;
         }
     }
 
-    QMesh* new_mesh = new QMesh;
     //Qt3DRender::QGeometry *newGeometry = new Qt3DRender::QGeometry;
 
     /*QMap<Qt3DRender::QBuffer *, Qt3DRender::QBuffer *> bufferMap;
@@ -92,7 +90,6 @@ GLModel::GLModel(QNode *parent)
         //qDebug() << "asdf" << att ;
     }*/
 
-    //m_mesh->setGeometry(m_geometry);
     //m_mesh->geometry()
 
     //qDebug() << "done loading mesh";
@@ -149,13 +146,18 @@ void GLModel::initialize(){
     m_geometry = new QGeometry(m_geometryRenderer);
     QByteArray vertexArray;
     vertexArray.resize(mesh->vertices.size()*5*3*3*sizeof(float));
-    reVertexArray = reinterpret_cast<float*>(vertexArray.data());
 
     vertexBuffer = new Qt3DRender::QBuffer(Qt3DRender::QBuffer::VertexBuffer,m_geometry);
     vertexBuffer->setUsage(Qt3DRender::QBuffer::DynamicDraw);
     vertexBuffer->setData(vertexArray);
 
-    // Attributes
+    QByteArray vertexNormalArray;
+    vertexNormalArray.resize(mesh->vertices.size()*5*3*1*sizeof(float));
+    vertexNormalBuffer = new Qt3DRender::QBuffer(Qt3DRender::QBuffer::VertexBuffer,m_geometry);
+    vertexNormalBuffer->setUsage(Qt3DRender::QBuffer::DynamicDraw);
+    vertexNormalBuffer->setData(vertexNormalArray);
+
+    // vertex Attributes
     positionAttribute = new QAttribute(m_geometry);
     positionAttribute->setAttributeType(QAttribute::VertexAttribute);
     positionAttribute->setBuffer(vertexBuffer);
@@ -163,10 +165,22 @@ void GLModel::initialize(){
     positionAttribute->setDataSize(3);
     positionAttribute->setByteOffset(0);
     positionAttribute->setByteStride(3*sizeof(float));
-    positionAttribute->setCount(2);
+    positionAttribute->setCount(mesh->vertices.size());
     positionAttribute->setName(QAttribute::defaultPositionAttributeName());
 
+    // normal Attributes
+    normalAttribute = new QAttribute(m_geometry);
+    normalAttribute->setAttributeType(QAttribute::VertexAttribute);
+    normalAttribute->setBuffer(vertexNormalBuffer);
+    normalAttribute->setDataType(QAttribute::Float);
+    normalAttribute->setDataSize(3);
+    normalAttribute->setByteOffset(0);
+    normalAttribute->setByteStride(3*sizeof(float));
+    normalAttribute->setCount(mesh->vertices.size());
+    normalAttribute->setName(QAttribute::defaultNormalAttributeName());
+
     m_geometry->addAttribute(positionAttribute);
+    m_geometry->addAttribute(normalAttribute);
 
     m_geometryRenderer->setInstanceCount(1);
     m_geometryRenderer->setFirstVertex(0);
@@ -219,10 +233,15 @@ void GLModel::addVertices(){
             result_vs.push_back(mesh->vertices[mf.mesh_vertex[fn]].position);
         }
         //addVertices(temp_face);
-        QVector3D norm_v = QVector3D::normal(result_vs[0], result_vs[1], result_vs[2]);
 
-        result_vs.push_back(norm_v);
+        QVector3D norm_v = QVector3D::normal(result_vs[2], result_vs[1], result_vs[0]);
+        vector<QVector3D> result_vns;
+        for(int i=0; i<1; i++){
+            result_vns.push_back(norm_v);
+        }
+
         addVertices(result_vs);
+        addNormalVertices(result_vns);
     }
 }
 
@@ -248,6 +267,54 @@ void GLModel::addVertices(vector<QVector3D> vertices){
     return;
 }
 
+void GLModel::addNormalVertices(vector<QVector3D> vertices){
+
+    int vertex_cnt = vertices.size();
+    //update geometry
+    QByteArray appendVertexArray;
+    appendVertexArray.resize(vertex_cnt*3*sizeof(float));
+    float* reVertexArray = reinterpret_cast<float*>(appendVertexArray.data());
+
+    for (int i=0; i<vertex_cnt; i++){
+        //coordinates of left vertex
+        reVertexArray[i*3+0] = vertices[i].x();
+        reVertexArray[i*3+1] = vertices[i].y();
+        reVertexArray[i*3+2] = vertices[i].z();
+    }
+
+
+    uint vertexCount = normalAttribute->count();
+    vertexNormalBuffer->updateData(vertexCount*3*sizeof(float),appendVertexArray);
+    normalAttribute->setCount(vertexCount+vertex_cnt);
+    return;
+}
+
+// need to create new mesh object liek Mesh* leftMesh = new Mesh();
+void GLModel::bisectModel(Mesh* mesh, Plane plane, Mesh* leftMesh, Mesh* rightMesh){
+    // do bisecting mesh
+    foreach (MeshFace mf, mesh->faces){
+        bool faceLeftToPlane = true;
+        for (int vn=0; vn<3; vn++){
+            MeshVertex mv = mesh->vertices[mf.mesh_vertex[vn]];
+            if (!isLeftToPlane(plane, mv.position)) // if one vertex is left to plane, append to left vertices part
+                faceLeftToPlane = false;
+        }
+        if (faceLeftToPlane)
+            leftMesh->addFace(mesh->vertices[mf.mesh_vertex[0]].position, mesh->vertices[mf.mesh_vertex[1]].position, mesh->vertices[mf.mesh_vertex[2]].position);
+        else
+            rightMesh->addFace(mesh->vertices[mf.mesh_vertex[0]].position, mesh->vertices[mf.mesh_vertex[1]].position, mesh->vertices[mf.mesh_vertex[2]].position);
+    }
+
+    leftMesh->connectFaces();
+    rightMesh->connectFaces();
+}
+
+bool GLModel::isLeftToPlane(Plane plane, QVector3D position){
+    // determine if position is left to plane or not
+    if (QVector3D::dotProduct(QVector3D::normal(plane[0], plane[1], plane[2]), position)>0)
+        return false;
+    return true;
+}
 
 void GLModel::onTimerUpdate()
 {
