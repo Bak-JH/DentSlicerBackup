38ef7ae01ec0646ec77d082670b4191b5c7dd85b
Yoon <hsy6119@naver.com>
2018-02-11 23:10:18 +0900

    pathsToMesh removed, getPathHead improved

--- arrange.cpp
+++ arrange.cpp
@@ -8,18 +8,18 @@ Arrange::Arrange()
 bool shit = false;
 
 Paths spreadingCheck(Mesh* mesh, bool* check, int chking_start){
-    //qDebug() << "SpreadingCheck started from" << chking_start;
+    /**/qDebug() << "SpreadingCheck started from" << chking_start;
     Paths paths;
     int chking = -1;
     bool outer_check[mesh->faces.size()] = {false};
     vector<MeshFace*> to_check;
     to_check.push_back(& mesh->faces[chking_start]);
     while(to_check.size()>0){
-        //qDebug() << "New spreadingCheck generation (" << to_check.size() << "faces)";
+        /**/qDebug() << "New spreadingCheck generation (" << to_check.size() << "faces)";
         vector<MeshFace*> next_to_check;
         for(int i=0; i<to_check.size(); i++){
             chking = to_check[i]->idx;
-            /*Debug
+            //*Debug
             qDebug() << "to_check" << i << "(Face" << chking << ")";
             for(int side=0; side<3; side++){
                 if (isEdgeBound(to_check[i], side)){
@@ -51,26 +51,18 @@ Paths spreadingCheck(Mesh* mesh, bool* check, int chking_start){
 }
 
 int getPathHead(MeshFace* mf, int side){
-    int path_head = mf->mesh_vertex[side];//set end point of the bound edges as path_head, working with following if statement
     if(side==0 && isEdgeBound(mf, 2)) {
-        if(isEdgeBound(mf, 1)){//all side of chking face is bound, face is alone
-            path_head = -1;
-        }
-        else path_head = mf->mesh_vertex[2];//reset path_head
+        if(isEdgeBound(mf, 1)) return -1;//all side of chking face is bound, face is alone
+        else return mf->mesh_vertex[2];
     }
-    return path_head;
+    return mf->mesh_vertex[side];
 }
 
 Path buildOutline(Mesh* mesh, bool* check, bool* outer_check, int chking, int path_head){
-    /*Debug
+    //*Debug
     qDebug() << "buildOutline from" << chking;
     //*/
     if(path_head==-1){
-        /*Debug
-        qDebug() << "!!!" << chking
-                 << cfg->resolution * mesh->vertices[mesh->faces[chking].mesh_vertex[0]].position.x()
-                 << cfg->resolution * mesh->vertices[mesh->faces[chking].mesh_vertex[0]].position.y();
-        //*/
         outer_check[chking] = true;
         check[chking] = true;
         MeshFace* mf = & mesh->faces[chking];
@@ -80,6 +72,7 @@ Path buildOutline(Mesh* mesh, bool* check, bool* outer_check, int chking, int pa
             QVector3D vertex = mesh->vertices[v_idx].position;
             mesh->Mesh::addPoint(vertex.x(), vertex.y(), &path);
         }
+        qDebug() << "buildOutline done";
         return path;
     }
     vector<int> path_by_idx;
@@ -88,6 +81,7 @@ Path buildOutline(Mesh* mesh, bool* check, bool* outer_check, int chking, int pa
     int from = -1;
     int path_tail = path_head;
     while(!outline_closed){
+        qDebug() << outer_chking;
         MeshFace* mf = & mesh->faces[outer_chking];
         int outline_edge_cnt = 0;
         int path_tail_idx;
@@ -126,6 +120,7 @@ Path buildOutline(Mesh* mesh, bool* check, bool* outer_check, int chking, int pa
         }
         if(path_tail == path_head) outline_closed = true;
     }
+    qDebug() << "buildOutline done";
     return idxsToPath(mesh, path_by_idx);
 }
 
@@ -194,66 +189,6 @@ Paths clipOutlines(vector<Paths> outline_sets){
         tmp_clip_result.clear();*/
     }
     return projection;
-}
-
-Mesh pathsToMesh(Paths paths){
-    paths = resize(paths);
-    Mesh mesh;
-    float thickness = 0.1;
-    for (int i=0; i<paths.size(); i++){
-        /****/qDebug() << "PathsToMesh Path" << i;
-        for (int j=0; j<paths[i].size(); j++){
-            /****/qDebug() << "PathsToMesh edge" << j;
-            float x1 = paths[i][j].X/1000, y1 = paths[i][j].Y/1000;
-            float x2 = paths[i][(j+1)%paths[i].size()].X/1000, y2 = paths[i][(j+1)%paths[i].size()].Y/1000;
-            float vec_x = thickness*(x2-x1)/sqrt(pow(x2-x1,2)+pow(y2-y1,2));
-            float vec_y = thickness*(y2-y1)/sqrt(pow(x2-x1,2)+pow(y2-y1,2));
-            /****/qDebug() << x1 << y1 <<x2 << y2 << vec_x << vec_y;
-            QVector3D v0 = QVector3D(x1, y1, 0);
-            QVector3D v1 = QVector3D(x2, y2, 0);
-            QVector3D v2 = QVector3D(x1+vec_y, y1-vec_x, 0);
-            QVector3D v3 = QVector3D(x2+vec_y, y2-vec_x, 0);
-            mesh.addFace(v2, v1, v0);
-            mesh.addFace(v1, v2, v3);
-        }
-    }
-    mesh.connectFaces();
-    cfg->origin = QVector3D((mesh.x_min+mesh.x_max)/2, (mesh.y_min+mesh.y_max)/2, (mesh.z_min+mesh.z_max)/2);
-    /****/qDebug() << "PathsToMesh done";
-    return mesh;
-}
-
-Paths resize(Paths paths){
-    float max_x = paths[0][0].X, min_x = paths[0][0].X, max_y = paths[0][0].Y, min_y = paths[0][0].Y;
-    for (int i=0; i<paths.size(); i++){
-        for (int j=0; j<paths[i].size(); j++){
-            if(paths[i][j].X>max_x) max_x=paths[i][j].X;
-            else if(paths[i][j].X<min_x) min_x=paths[i][j].X;
-            if(paths[i][j].Y>max_y) max_y=paths[i][j].Y;
-            else if(paths[i][j].Y<min_y) min_y=paths[i][j].Y;
-        }
-    }
-
-    qDebug() << "max_x" << max_x << "min_x" << min_x;
-    qDebug() << "max_y" << max_y << "min_y" << min_y;
-
-    float center_x = 0, center_y = 0;
-    float max_length = 60*1000;
-    float move_x, move_y, multiplying_factor;
-    move_x = center_x - (max_x + min_x)/2;
-    move_y = center_y - (max_y + min_y)/2;
-    if (max_x - min_x > max_y - min_y) multiplying_factor = max_length / (max_x - min_x);
-    else multiplying_factor = max_length / (max_y - min_y);
-
-    qDebug() << "move_x" << move_x << "move_y" << move_y << "multiplying_factor" << multiplying_factor;
-
-    for (int i=0; i<paths.size(); i++){
-        for (int j=0; j<paths[i].size(); j++){
-            paths[i][j].X = (paths[i][j].X + move_x) * multiplying_factor;
-            paths[i][j].Y = (paths[i][j].Y + move_y) * multiplying_factor;
-        }
-    }
-    return paths;
 }
 
 void debugPath(Paths paths){
