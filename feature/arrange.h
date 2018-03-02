fb83a1a018f060245f3125b8c2052375cdaa718d
Yoon <hsy6119@naver.com>
2018-03-02 16:55:03 +0900

    arrange debug functions improved _ 2

--- feature/arrange.h
+++ feature/arrange.h
@@ -32,5 +32,6 @@ Paths resize(Paths paths);
 void debugPath(Paths paths);
 void debugFaces(Mesh* mesh, vector<int> face_list);
 void debugFace(Mesh* mesh, int face_idx);
-int findVertexWithIntpoint(int x, int y, Mesh* mesh);
+int findVertexWithIntpoint(IntPoint p, Mesh* mesh);
+int findVertexWithIntXY(int x, int y, Mesh* mesh);
 #endif // ARRANGE_H
