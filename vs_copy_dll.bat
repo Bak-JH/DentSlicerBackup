04e3cf5b3581f12be8c4a03371c040f7d4287214
Won Bin Kang <legokangpalla@gmail.com>
2019-04-23 18:32:00 +0900

    build bat

--- vs_copy_dll.bat
+++ vs_copy_dll.bat
@@ -1,3 +1,3 @@
-xcopy /s/Y %~dp0\utils\winsparkle\WinSparkle.dll %~dp0\debug
-xcopy /s/Y %~dp0\utils\winsparkle\WinSparkle.lib %~dp0\debug
-xcopy /s/Y %~dp0\utils\winsparkle\WinSparkle.pdb %~dp0\debug
+xcopy /s/Y %~dp0\utils\winsparkle\WinSparkle.dll %~dp0\%1
+xcopy /s/Y %~dp0\utils\winsparkle\WinSparkle.lib %~dp0\%1
+xcopy /s/Y %~dp0\utils\winsparkle\WinSparkle.pdb %~dp0\%1
