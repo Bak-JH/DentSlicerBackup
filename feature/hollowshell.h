8950352a1d1cf0e4d36b9ba11d658bb58a78bf3e
Won Bin Kang <legokangpalla@gmail.com>
2019-04-25 11:33:32 +0900

    wip

--- feature/hollowshell.h
+++ feature/hollowshell.h
@@ -2,12 +2,11 @@
 #define HOLLOWSHELL_H
 #include "DentEngine/src/mesh.h"
 
-class HollowShell
+namespace HollowShell
 {
-public:
-	static void hollowShell(Mesh* mesh, MeshFace* mf, QVector3D center, float radius);
-	static void detectHollowShellFaces(Mesh* mesh, QVector3D normal, MeshFace* original_mf, MeshFace* mf, std::vector<MeshFace*>* result);
-	static void offsetHollowShellFaces(Mesh* mesh, std::vector<MeshFace*>* hollow_shell_faces, QVector3D center, float radius);
+	void hollowShell(Mesh* mesh, MeshFace* mf, QVector3D center, float radius);
+	void detectHollowShellFaces(Mesh* mesh, QVector3D normal, const MeshFace* original_mf,const MeshFace* mf, std::vector<const MeshFace*>* result);
+	void offsetHollowShellFaces(Mesh* mesh, std::vector<const MeshFace*>* hollow_shell_faces, QVector3D center, float radius);
 
 };
 
