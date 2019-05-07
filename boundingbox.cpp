03dcd5215fe7cba070310da1eea0f13c9de06078
legokangpalla <legokangpalla@gmail.com>
2019-05-07 10:58:19 +0900

    wip, bounding box overhaul

--- boundingbox.cpp
+++ boundingbox.cpp
@@ -7,25 +7,23 @@ using namespace Qt3DExtras;
 
 BoundingBox::BoundingBox(QNode* parent): QEntity(parent)
 {
-	auto mat = new QPhongMaterial();
-	mat->setAmbient(QColor(240, 249, 70, 255));
-	mat->setDiffuse(QColor(240, 249, 70, 255));
-	mat->setSpecular(QColor(240, 249, 70, 255));
-	mat->setShininess(0);
-	auto mesh = new QMesh();
-	mesh->setSource(QUrl("qrc:/Resource/mesh/boundedBox3.stl"));
+    auto rect = new QCuboidMesh();
+    rect->setXExtent(1);
+    rect->setYExtent(1);
+    rect->setZExtent(1);
+
+    rect->setXYMeshResolution({2,2});
+    rect->setXZMeshResolution({2,2});
+    rect->setYZMeshResolution({2,2});
 	_transform = new Qt3DCore::QTransform();
 	_transform->setScale(1.0f);
-	this->addComponent(mesh);
+	this->addComponent(rect);
 	this->addComponent(_transform);
-	this->addComponent(mat);
-
 }
 
 void BoundingBox::setSize(const QVector3D& size)
 {
 	_transform->setScale3D(size);
-
 }
 
 void BoundingBox::setPos(const QVector3D& pos)
