06ba04e66729610222973e022e3be94bd8e95c00
diridiri <junharin@kaist.ac.kr>
2019-06-28 21:05:52 +0900

    ADDED vitro version config

--- DentEngine/src/configuration.h
+++ DentEngine/src/configuration.h
@@ -77,12 +77,19 @@ public:
     int bed_x = 120.96; // in mm
     int bed_y = 68.04; // in mm*/
 
-    // settings for vittro
+    /*// settings for vittro
     int resolution_x = 1920;
     int resolution_y = 1080;
 
     int bed_x = 124.8; // in mm
-    int bed_y = 70.2; // in mm
+    int bed_y = 70.2; // in mm*/
+
+    // settings for vittro
+    int resolution_x = 1280;
+    int resolution_y = 800;
+
+    int bed_x = 138; // in mm
+    int bed_y = 90; // in mm
 
 	size_t bed_margin_x = 1;
 	size_t bed_margin_y = 1;
