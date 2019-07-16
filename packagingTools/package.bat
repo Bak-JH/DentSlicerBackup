13a0e6cb0a4d5f7e9adbc96902501fe7f1751648
Won Bin Kang <legokangpalla@gmail.com>
2019-07-16 11:35:21 +0900

    added script to copy mingw libs.

--- packagingTools/package.bat
+++ packagingTools/package.bat
@@ -3,6 +3,6 @@ ECHO Welcome to DSP: DentSlicer Packager
 ECHO DSP: running windeployqt
 %DS_COMPILER_DIR%\windeployqt.exe --dir %DS_PACKAGE_INPUT% --compiler-runtime --release --qmldir %DS_PACKAGE_SOURCE%\Qml %DS_RELEASE_BUILD%\DentSlicer.exe
 ECHO running vs_copy_dll for copying dependent libraries
-start %DS_PACKAGE_SOURCE%\vs_copy_dll.bat release %DS_PACKAGE_INPUT% %DS_PACKAGE_SOURCE%
+start vs_copy_dll_mingw.bat release %DS_PACKAGE_INPUT% %DS_PACKAGE_SOURCE%
 ECHO copy DentSlicer.exe
 xcopy /s/Y %DS_RELEASE_BUILD%\DentSlicer.exe %DS_PACKAGE_INPUT%
