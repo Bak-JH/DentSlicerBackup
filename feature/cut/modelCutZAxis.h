25ca99e5d0cb582554371976d020306db50b9807
legokangpalla <legokangpalla@gmail.com>
2019-09-15 20:11:08 +0900

    z axis cut wip.

---
+++ feature/cut/modelCutZAxis.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "../../common/Task.h"
+#include "../../DentEngine/src/mesh.h"
+#include "../../DentEngine/src/ContourBuilder.h"
+
+namespace Hix
+{
+	namespace Engine3D
+	{
+		class Mesh;
+	}
+	namespace Features
+	{
+		namespace Cut
+		{
+			class ZAxisCutTask: public Hix::Tasking::Task
+			{
+			public:
+				ZAxisCutTask(const Engine3D::Mesh* originalMesh, float z);
+				tf::Taskflow& getFlow()override;
+
+			private:
+				void divideTriangles();
+				void generateCutContour();
+				void generateCaps();
+
+
+				float _cuttingPlane;
+				const Engine3D::Mesh* _origMesh = nullptr;
+				Engine3D::Mesh* _bottomMesh = nullptr;
+				Engine3D::Mesh* _topMesh = nullptr;
+
+				std::unordered_set<Hix::Engine3D::FaceConstItr> _botFaces;
+				std::unordered_set<Hix::Engine3D::FaceConstItr> _overlapFaces;
+				std::unordered_set<Hix::Engine3D::FaceConstItr> _topFaces;
+				std::vector<Hix::Slicer::Contour> _contours;
+				
+
+			};
+		}
+	}
+}
--- feature/modelCutZAxis.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Hix
-{
-	namespace ModelCutZAxis
-	{
-		void 
-	}
-}
