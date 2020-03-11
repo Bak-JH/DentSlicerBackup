c44f426d622ac63a290478d2a75f82376eac5ec8
legokangpalla <legokangpalla@gmail.com>
2020-03-11 19:14:00 +0900

    cut bug

--- feature/cut/modelcut.cpp
+++ feature/cut/modelcut.cpp
@@ -55,7 +55,7 @@ void ModelCut::cutModeSelected()
 		_cuttingPlane.transform().setTranslation(QVector3D(0, 0, _modelsBound.zMin() + 1 * _modelsBound.lengthZ() / 1.8));
 		_cuttingPlane.enablePlane(true);
 		Hix::Application::ApplicationManager::getInstance().sceneManager().setViewPreset(Hix::Application::SceneManager::ViewPreset::Center);
-		setSliderVisible(false);
+		setSliderVisible(true);
 	}
 	else if (_cutSwitch->isChecked())
 	{
@@ -67,7 +67,7 @@ void ModelCut::cutModeSelected()
 		_cuttingPlane.enablePlane(true);
 		Hix::Application::ApplicationManager::getInstance().getRayCaster().setHoverEnabled(true);
 		Hix::Application::ApplicationManager::getInstance().sceneManager().setViewPreset(Hix::Application::SceneManager::ViewPreset::Down);
-		setSliderVisible(true);
+		setSliderVisible(false);
 	}
 	return;
 }
