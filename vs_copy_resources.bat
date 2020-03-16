615ef1bf340873e2fa1358ae87fa9aa3fdaf82b3
Bak-JH <bakjh.6280@gmail.com>
2020-03-16 14:27:17 +0900

    packaging path edited

--- vs_copy_resources.bat
+++ vs_copy_resources.bat
@@ -1,7 +1,7 @@
 Rem settings
-xcopy /Y %3\settings.json %2
-xcopy /Y %3\sliceSettings.json %2
-xcopy /Y %3\supportSettings.json %2
+xcopy /Y %3settings.json %2
+xcopy /Y %3sliceSettings.json %2
+xcopy /Y %3supportSettings.json %2
 
 xcopy /s/Y %3\PrinterPresets %2\PrinterPresets /i
 
