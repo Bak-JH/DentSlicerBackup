fb59b98490550298f0eb7da3efaea32e07283a14
legokangpalla <legokangpalla@gmail.com>
2020-03-20 14:06:56 +0900

    layerview finished, minor support highlight bugs fixed

--- feature/layerview/layerview.cpp
+++ feature/layerview/layerview.cpp
@@ -4,7 +4,10 @@
 #include "../cut/modelcut.h"
 #include "../../glmodel.h"
 #include "../../support/SupportModel.h"
+#include "../../support/RaftModel.h"
+
 using namespace Hix;
+using namespace Hix::Render;
 using namespace Hix::Features;
 using namespace Hix::Features::Cut;
 using namespace Hix::Engine3D;
@@ -21,7 +24,7 @@ Hix::Features::LayerView::LayerView():
 	slider().setValue(slider().getMax());
 	slider().setVisible(true);
 
-
+	//change shader, change support, raft to same color as model
 	for (auto& model : _models)
 	{
 		model->setMaterialMode(Hix::Render::ShaderMode::LayerMode);
@@ -31,9 +34,9 @@ Hix::Features::LayerView::LayerView():
 		_modelColorMap[model] = Hix::Render::Colors::Selected;
 	}
 	auto supports = Hix::Application::ApplicationManager::getInstance().supportRaftManager().modelAttachedSupports(_models);
-	
 	for (auto& s : supports)
 	{
+		s->setMaterialColor(Hix::Render::Colors::Selected);
 		s->setMaterialMode(Hix::Render::ShaderMode::LayerMode);
 		s->updateMesh();
 		s->setMaterialParamter("fuckingStuipidWorldMatrix", QVariant::fromValue(s->toRootMatrix()));
@@ -41,27 +44,40 @@ Hix::Features::LayerView::LayerView():
 		_modelColorMap[s] = Hix::Render::Colors::Support;
 
 	}
+	auto raft = Hix::Application::ApplicationManager::getInstance().supportRaftManager().raftModel();
+	if(raft)
+	{
+		raft->setMaterialColor(Hix::Render::Colors::Selected);
+		raft->setMaterialMode(Hix::Render::ShaderMode::LayerMode);
+		raft->updateMesh();
+		raft->setMaterialParamter("fuckingStuipidWorldMatrix", QVariant::fromValue(raft->toRootMatrix()));
+		raft->setMaterialParamter("height", QVariant::fromValue((float)slider().getMax()));
+		_modelColorMap[raft] = Hix::Render::Colors::Raft;
+	}
+
 	QObject::connect(&slider(), &Hix::QML::SlideBarShell::valueChanged, [this]() {
 		_crossSectionPlane.showLayer(slider().getValue());
 		float h = slider().getValue();
-		for (auto& model : _models)
+		for (auto& pair : _modelColorMap)
 		{
-			model->setMaterialParamter("height", QVariant::fromValue(h));
+			pair.first->setMaterialParamter("height", QVariant::fromValue(h));
 		}
 		});
 	
 	//prepare layers
-	Hix::Application::ApplicationManager::getInstance().taskManager().enqueTask(std::make_unique<LayerviewPrep>(_models, _crossSectionPlane));
+	Hix::Application::ApplicationManager::getInstance().taskManager().enqueTask(std::make_unique<LayerviewPrep>(_modelColorMap, _crossSectionPlane));
 
 
 }
 
 Hix::Features::LayerView::~LayerView()
 {
-	for (auto& model : _models)
+	//rever colors and shader
+	for (auto& pair : _modelColorMap)
 	{
-		model->setMaterialMode(Hix::Render::ShaderMode::SingleColor);
-		model->updateMesh();
+		pair.first->setMaterialMode(Hix::Render::ShaderMode::SingleColor);
+		pair.first->setMaterialColor(pair.second);
+		pair.first->updateMesh();
 	}
 }
 
@@ -85,8 +101,13 @@ Hix::Features::LayerviewPrep::~LayerviewPrep()
 void Hix::Features::LayerviewPrep::run()
 {
 	_crossSec.init(_modelColorMap);
-	postUIthread([this]() {
-		float h = Hix::Engine3D::combineBounds(_models).zMax();
+	std::unordered_set<SceneEntity*> entities;
+	for (auto& pair : _modelColorMap)
+	{
+		entities.insert(pair.first);
+	}
+	postUIthread([this, &entities]() {
+		float h = Hix::Engine3D::combineBounds(entities).zMax();
 		_crossSec.showLayer(h);
 		});
 }
