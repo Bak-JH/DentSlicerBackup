f2b281067cfdbb493b73526f1ead76112fa2d96c
legokangpalla <legokangpalla@gmail.com>
2020-04-06 20:06:27 +0900

    wip2

--- feature/agCDT/CDT.h
+++ feature/agCDT/CDT.h
@@ -50,13 +50,13 @@ public:
     void insertVertices(const std::vector<V2d<T> >& vertices);
     /// Add constraints (fixed edges) to triangulation
     void insertEdges(const std::vector<Edge>& edges);
-    // same as above, skip faulty edges
-    void tryInsertEdges(const std::vector<Edge>& edges);
-
     /// Erase triangles adjacent to super triangle
     void eraseSuperTriangle();
     /// Erase triangles outside of constrained boundary using growing
     void eraseOuterTriangles();
+    /// Erase triangles outside of constrained boundary
+    /// and automatically detected holes using growing
+    void eraseOuterTrianglesAndHoles();
 
 private:
     /*____ Detail __*/
@@ -113,6 +113,12 @@ private:
     void makeDummy(const TriInd iT);
     void eraseDummies();
     void eraseSuperTriangleVertices();
+    template <typename TriIndexIter>
+    void eraseTrianglesAtIndices(TriIndexIter first, TriIndexIter last);
+    TriIndUSet growToBoundary(std::stack<TriInd> seeds) const;
+    // return triangles behind boundary as second out parameter
+    std::pair<TriIndUSet, TriIndUSet>
+    growToBoundaryExt(std::stack<TriInd> seeds, TriIndUSet& traversed) const;
 
     std::vector<TriInd> m_dummyTris;
 #ifndef CDT_DONT_USE_BOOST_RTREE
