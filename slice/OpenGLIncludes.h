013af5feea51ef4b2b07278111be531349a62832
legokangpalla <legokangpalla@gmail.com>
2020-06-03 00:24:29 +0900

    works, minor bugs.

--- slice/OpenGLIncludes.h
+++ slice/OpenGLIncludes.h
@@ -2,8 +2,7 @@
 //#include <GL/glew.h> // Before any gl headers
 //#include <GL/gl.h>
 
+#include <glad/glad.h>
 
 //#include <GL/glu.h> // Always after gl.h
 #include <GLFW/glfw3.h> // When all gl-headers have been included
-#include <QOpenGLFunctions>
-#include <QOpenGLExtraFunctions>
