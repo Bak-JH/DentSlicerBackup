df17165bdfac871f89d0e822570e299ac63b7a28
legokangpalla <legokangpalla@gmail.com>
2020-06-04 01:35:19 +0900

    various bugs fixed.

--- slice/InfoWriter.h
+++ slice/InfoWriter.h
@@ -15,10 +15,11 @@ namespace Hix
 		private:
 			QString _outfoldername;
 			QString _infoJsonName;
-			size_t _resX, _resY, _layerHeight;
+			size_t _resX, _resY;
+			float _layerHeight;
 
 		public:
-			InfoWriter(QString outFolder, size_t resX, size_t resY, size_t layerHeight);
+			InfoWriter(QString outFolder, size_t resX, size_t resY, float layerHeight);
 			void createInfoFile();
 			void writeVittroOptions(int max_slices, const Hix::Engine3D::Bounds3D& bound);
 			void writeBasicInfo(int sliceCnt, const std::optional<rapidjson::Value>& optionalVal);
