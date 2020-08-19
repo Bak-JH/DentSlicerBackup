fd20bb8fba2a521cc416c8779d98d667a669894f
legokangpalla <legokangpalla@gmail.com>
2020-08-19 18:18:31 +0900

    added zip slice, changed capsule names and non-existing printers.

--- slice/InfoWriter.h
+++ slice/InfoWriter.h
@@ -13,13 +13,14 @@ namespace Hix
 		class InfoWriter
 		{
 		private:
-			QString _outfoldername;
-			QString _infoJsonName;
+			//QString _outfoldername;
+			std::filesystem::path _outfoldername;
+			std::filesystem::path _infoJsonName;
 			size_t _resX, _resY;
 			float _layerHeight;
 
 		public:
-			InfoWriter(QString outFolder, size_t resX, size_t resY, float layerHeight);
+			InfoWriter(std::filesystem::path outFolder, size_t resX, size_t resY, float layerHeight);
 			void createInfoFile();
 			void writeVittroOptions(int max_slices, const Hix::Engine3D::Bounds3D& bound);
 			void writeBasicInfo(int sliceCnt, const std::optional<rapidjson::Value>& optionalVal);
