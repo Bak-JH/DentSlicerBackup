150f390de34f6f3af0b115cca6cbee4b8f29ea49
Bak-JH <bakjh.6280@gmail.com>
2020-10-07 20:08:54 +0900

    refactor shader name

--- render/ModelMaterial.cpp
+++ render/ModelMaterial.cpp
@@ -147,7 +147,7 @@ void  Hix::Render::ModelMaterial::changeMode(ShaderMode mode) {
 		}
 		else if (_mode == ShaderMode::NoLightsMode)
 		{
-			removeParameterWithKey("lineColor");
+			removeParameterWithKey("singleColor");
 		}
 		switch (mode) {
 		case ShaderMode::SingleColor: // default
@@ -165,8 +165,8 @@ void  Hix::Render::ModelMaterial::changeMode(ShaderMode mode) {
 			addParameterWithKey("fuckingStuipidWorldMatrix");
 			break; 
 		case ShaderMode::NoLightsMode:
-			_shaderProgram.setFragmentShaderCode(QShaderProgram::loadSource(QUrl("qrc:/shaders/lineColor.frag")));
-			addParameterWithKey("lineColor");
+			_shaderProgram.setFragmentShaderCode(QShaderProgram::loadSource(QUrl("qrc:/shaders/singleColorNoLight.frag")));
+			addParameterWithKey("singleColor");
 			break;
 		default:
 			break;
