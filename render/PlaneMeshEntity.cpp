343a3d97fb9918789155d0f40b8d48f4c676a5ab
Bak-JH <bakjh.6280@gmail.com>
2020-10-15 01:32:10 +0900

    add setAlpha function in PlaneMeshEntity. but there is an alpha value problem with cutting plane....

--- render/PlaneMeshEntity.cpp
+++ render/PlaneMeshEntity.cpp
@@ -3,13 +3,13 @@
 
 using namespace Hix::Render;
 
-PlaneMeshEntity::PlaneMeshEntity(Qt3DCore::QEntity* owner, bool isDoubleSided, float alpha):
+PlaneMeshEntity::PlaneMeshEntity(Qt3DCore::QEntity* owner, bool isDoubleSided):
 	PlaneMeshEntity(owner, Hix::Application::ApplicationManager::getInstance().settings().printerSetting.bedBound.lengthX(),
-		Hix::Application::ApplicationManager::getInstance().settings().printerSetting.bedBound.lengthY(), QColor(140, 140, 140, 255), isDoubleSided)
+		Hix::Application::ApplicationManager::getInstance().settings().printerSetting.bedBound.lengthY(), QColor(140, 140, 140, 0), isDoubleSided)
 {
 }
 
-Hix::Render::PlaneMeshEntity::PlaneMeshEntity(Qt3DCore::QEntity* owner, float width, float height, const QColor& color, bool isDoubleSided, float alpha) : Qt3DCore::QEntity(owner)
+Hix::Render::PlaneMeshEntity::PlaneMeshEntity(Qt3DCore::QEntity* owner, float width, float height, const QColor& color, bool isDoubleSided) : Qt3DCore::QEntity(owner)
 {
 	size_t plnCnt = 1;
 	if (isDoubleSided)
@@ -20,7 +20,6 @@ Hix::Render::PlaneMeshEntity::PlaneMeshEntity(Qt3DCore::QEntity* owner, float wi
 		planeMaterial->setAmbient(color);
 		planeMaterial->setDiffuse(color);
 		planeMaterial->setSpecular(color);
-		planeMaterial->setAlpha(alpha);
 		auto planeEntity = new Qt3DCore::QEntity(this);
 		//qDebug() << "generatePlane---------------------==========-=-==-" << parentModel;
 		auto clipPlane = new Qt3DExtras::QPlaneMesh(this);
@@ -37,6 +36,20 @@ Hix::Render::PlaneMeshEntity::PlaneMeshEntity(Qt3DCore::QEntity* owner, float wi
 	}
 
 	addComponent(&_transform);
+}
+
+void Hix::Render::PlaneMeshEntity::setAlpha(const float alpha)
+{
+	for (auto each : _meshTransformMap)
+	{
+		for (auto comp : each.first->components())
+		{
+			if (dynamic_cast<Qt3DExtras::QPhongAlphaMaterial*>(comp))
+			{
+				dynamic_cast<Qt3DExtras::QPhongAlphaMaterial*>(comp)->setAlpha(alpha);
+			}
+		}
+	}
 }
 
 PlaneMeshEntity::~PlaneMeshEntity()
