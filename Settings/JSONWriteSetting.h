14853a9800ab5d205c57ba83f74f364b5c8feb53
legokangpalla <legokangpalla@gmail.com>
2020-10-16 17:29:02 +0900

    simple max decimal digits limit instead

--- Settings/JSONWriteSetting.h
+++ Settings/JSONWriteSetting.h
@@ -1,24 +1,14 @@
 #pragma once
 #include <filesystem>
 #include "../common/rapidjson/document.h"
-#include "../common/rapidjson/writer.h"
-#include "../common/rapidjson/stringbuffer.h"
-#include "../common/rapidjson/PrettyWriter.h"
 #include <string>
 #include <unordered_map>
 #include <algorithm>
 #include <optional>
-#include <memory>
-
-//namespace rapidjson
-//{
-//	class StringBuffer;
-//}
 namespace Hix
 {
 	namespace Settings
 	{
-
 
 		class JSONWriteSetting
 		{
@@ -26,16 +16,8 @@ namespace Hix
 			virtual void writeJSON();
 		protected:
 			virtual const std::filesystem::path& jsonPath() = 0;
-			virtual void writeObjects() = 0;
-			std::unique_ptr< rapidjson::StringBuffer> _workingBuffer;
+			virtual rapidjson::Document doc() = 0;
 
-			//unlike parse, it's a member function
-			template<typename ValType>
-			void writeVal(const std::string& key, const ValType& val)
-			{
-				rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(_workingBuffer);
-				writer.write
-			}
 		};
 	}
 }
