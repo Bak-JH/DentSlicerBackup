20d9cdb681e05d515f3b1a2964643b2eb6dbd54f
legokangpalla <legokangpalla@gmail.com>
2021-01-21 18:00:20 +0900

    wip

--- src/common/frozen/meta/string_without_boost.hpp
+++
@@ -1,9 +0,0 @@
-#if !defined FROZEN_META_STRING_WITHOUT_BOOST_HPP_INCLUDED
-#define      FROZEN_META_STRING_WITHOUT_BOOST_HPP_INCLUDED
-
-#include "./string/basic_string.hpp"
-#include "./string/string_aliases.hpp"
-#include "./string/operations.hpp"
-#include "./string/to_string.hpp"
-
-#endif    // FROZEN_META_STRING_WITHOUT_BOOST_HPP_INCLUDED
