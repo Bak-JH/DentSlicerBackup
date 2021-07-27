b749500486eb24c432c0ac214ec7810aced3e2d6
BakJH <bakjh.6280@gmail.com>
2021-07-27 17:03:28 +0900

    wip

--- common/debugging/DebugRenderObject.h
+++ common/debugging/DebugRenderObject.h
@@ -4,11 +4,13 @@
 #include "../Singleton.h"
 #include "../../render/LineMeshEntity.h"
 #include "../../render/PlaneMeshEntity.h"
+#include "render/PointMeshEntity.h"
 #include "../../render/Color.h"
 #include "../../feature/Plane3D.h"
 #include "../../Mesh/mesh.h"
 #include <optional>
 #include <functional>
+#include "../../render/Bounds3D.h"
 //#include "glmodel.h"
 
 namespace Qt3DCore
@@ -35,12 +37,15 @@ namespace Hix
 		{
 		public:
 			void initialize(Qt3DCore::QEntity* root);
-			void addPath(const std::vector<QVector3D>& vertices, const QColor& color = QColor(0,0,0, 255));
-			void addPaths(const std::vector< std::vector<QVector3D>>& paths, const QColor& color = QColor(0, 0, 0, 255));
+			void addVertex(const QVector3D vertex, const QColor& color = QColor(0,0,0, 255));
+			void addVertices(const std::vector<QVector3D>& vertices, const QColor& color = QColor(0,0,0, 255));
+			void addLinePath(const std::vector<QVector3D>& vertices, const QColor& color = QColor(0,0,0, 255));
+			void addLinePaths(const std::vector< std::vector<QVector3D>>& paths, const QColor& color = QColor(0, 0, 0, 255));
 			void outlineFace(const Hix::Engine3D::FaceConstItr& face, const QColor& color = QColor(0, 0, 0, 255));
 			void registerDebugColorFaces(SceneEntityWithMaterial* owner, const std::unordered_set<Hix::Engine3D::FaceConstItr>& faces, const QVector4D& color);
 			void colorDebugFaces();
 			void showGLModelAabb(SceneEntityWithMaterial* target);
+			void showAabb(Hix::Engine3D::Bounds3D& aabb);
 			void clear();
 			std::optional<std::reference_wrapper<const FaceColorSet>>getOverrideColors(SceneEntityWithMaterial* owner)const;
 			Hix::Render::PlaneMeshEntity& displayPlane(const Hix::Plane3D::PDPlane& pdplane);
@@ -48,6 +53,7 @@ namespace Hix
 			std::unordered_map<SceneEntityWithMaterial*, FaceColorSet> _debugFaceMap;
 			std::deque<Hix::Render::LineMeshEntity> _lines;
 			std::deque<Hix::Render::PlaneMeshEntity>_planes;
+			std::deque<Hix::Render::PointMeshEntity>_points;
 			Qt3DCore::QEntity* _root = nullptr;
 		};
 	}
