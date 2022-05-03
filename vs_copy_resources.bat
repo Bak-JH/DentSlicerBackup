edf61c3d6d3c992e5646012c6b381bb86105d60d
BakJH <bakjh.6280@hix.co.kr>
2022-05-03 17:46:34 +0900

    packaged 1.2.9_rc4 / fixed copy path / added 1.2.9 patchnote

--- vs_copy_resources.bat
+++ vs_copy_resources.bat
@@ -3,7 +3,7 @@ xcopy /Y %3settings.json %2
 xcopy /Y %3sliceSettings.json %2
 xcopy /Y %3supportSettings.json %2
 xcopy /Y %3additionalSettings.json %2
-xcopy /Y %labelSettings.json %2
+xcopy /Y %3labelSettings.json %2
 xcopy /Y %3deploy.json %2
 xcopy /s/Y %3\PrinterPresets %2\PrinterPresets /i
 
