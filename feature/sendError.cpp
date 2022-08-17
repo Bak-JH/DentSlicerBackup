f22a8045b0d6ab4fc1b6f75e15a4a64378eb50e7
BakJH <bakjh.6280@hix.co.kr>
2022-08-17 17:55:40 +0900

    add send error feature

--- feature/sendError.cpp
+++ feature/sendError.cpp
@@ -1,6 +1,12 @@
-#include "../application/ApplicationManager.h"
+#include <boost/stacktrace.hpp>
+#include <fstream>
+#include <QNetworkReply>
 
+#include "application/ApplicationManager.h"
+#include "utils/CrashReport/version.h"
 #include "sendError.h"
+
+#include "utils/CrashReport/version.h"
 
 using namespace Hix;
 using namespace Hix::Render;
@@ -21,8 +27,40 @@ Hix::Features::SendError::~SendError()
 
 void Hix::Features::SendError::run()
 {
-	Sleep(3000);
 	postUIthread([]() {
+		//std::fstream file("D:\\err.txt", std::fstream::out);
+		//file << boost::stacktrace::to_string(boost::stacktrace::stacktrace());
+		auto stack = boost::stacktrace::to_string(boost::stacktrace::stacktrace());
+
+		QNetworkAccessManager* manager = new QNetworkAccessManager();
+		const QUrl url(CRASHREPORTER_SUBMIT_URL);
+		QNetworkRequest request(url);
+		request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+
+		QJsonObject obj;
+		obj["build_id"] = BUILD_ID;
+		obj["version"] = VER_FILEVERSION_STR;
+		obj["call-stack"] = QString::fromStdString(stack);
+
+		QJsonDocument doc(obj);
+		QByteArray data = doc.toJson();
+
+		QNetworkReply* reply = manager->post(request, data);
+
+		QObject::connect(reply, &QNetworkReply::finished, [=]() {
+			if (reply->error() == QNetworkReply::NoError) {
+				QString contents = QString::fromUtf8(reply->readAll());
+				qDebug() << contents;
+			}
+			else {
+				QString err = reply->errorString();
+				qDebug() << reply->error();
+			}
+			reply->deleteLater();
+			});
+
+
+
 		auto& progressManager = Hix::Application::ApplicationManager::getInstance().taskManager().progressManager();
 		progressManager.deletePopup();
 		});
